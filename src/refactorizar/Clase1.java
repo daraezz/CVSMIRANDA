14aa40859e95ba31453a699e040114c0c51ed09a
dam <dam@203P09>
2025-03-20 14:04:19 +0100

    Primer Commit

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    String nombre = "Daniel";
+    String apellido = "Miranda";
+}
