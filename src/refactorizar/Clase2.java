fc1b76aefd2126c07bdbbf9613885e860ceed0d1
dam <dam@203P09>
2025-03-20 14:10:52 +0100

    Añadida la clase2 con la rama versionBETA

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    String linea = "Estoy en la rama produccion";
+}
